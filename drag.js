60960748ad00a9b3854d639895d53176ec00d5d9
xueranbo <30611376+xueranbo@users.noreply.github.com>
2017-08-02 14:18:14 +0800

    Update drag.js

--- drag.js
+++ drag.js
@@ -6,10 +6,14 @@ function drag(id){
    var disy = 0;
    obj.onmousedown = function(ev){
       disx = ev.clientX - obj.offsetLeft;
-	  disy = ev.clientY - obj.ogdetTop;
+	  disy = ev.clientY - obj.offsetTop;
 	  document.onmousemove = function(ev){
 	    obj.style.left = ev.clientX - disx + 'px';
 		obj.style.top = ev.clientY - disy + 'px';
 	  }
+	  document.onmouseup = function(){
+	    document.onmousemove = document.onmouseup = null;
+	  }
+	  return false;
    }
 }
