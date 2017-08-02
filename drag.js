535938142837b0187d251ab30aa7de42f6155742
xueranbo <463060768@qq.com>
2017-08-02 10:03:35 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -4,7 +4,8 @@ function drag(id){
    var obj = document.getElementById(id);
    var disx = 0;
    var disy = 0;
-   obj.onmousedown = function(){
-
+   obj.onmousedown = function(ev){
+      disx = ev.clientX - obj.offsetLeft;
+	  disy = ev.clientY - obj.ogdetTop;
    }
 }
